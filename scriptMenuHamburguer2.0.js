6215aa4f9390eb9f6fce607a4beaec8119e71526
nettodefreitas7x0-netizen <netto.de.freitas7x0@gmail.com>
2026-09-09 22:59:52 -0300

    menu-buguer-mudouStyle.2.0

--- scriptMenuHamburguer2.0.js
+++ scriptMenuHamburguer2.0.js
@@ -118,14 +118,14 @@ window.addEventListener('scroll', function() {
         if (scrollTop > 100) {
         
            
-        // header.style.padding = '5px  5px 5px 10px';
-             headerContent.style.magin = '0px';
+        // headerContent.style.padding = '5px 10px';
+       
             // if (nav) nav.classList.add('nav-top');
                /// quando estar no topo da pagina
         } else {
-          
+            //  headerContent.style.padding = '20px 10px';
              
-               headerContent.style.magin = '0px 0';
+               
               
 
          
